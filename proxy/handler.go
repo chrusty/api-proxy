2d8f57aa8fc1a067c04aecb258bfa6655420c96c
Chris (CruftMaster) <chrusty@gmail.com>
2018-11-18 13:17:07 +0000

    Handling request and response bodies

--- proxy/handler.go
+++ proxy/handler.go
@@ -1,7 +1,9 @@
 package proxy
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
@@ -30,10 +32,20 @@ func (p *Proxy) handler(w http.ResponseWriter, req *http.Request) {
 	// Add this to the logger:
 	fieldLogger = fieldLogger.WithField("destination", destinationAddress)
 
+	// Read the request body:
+	requestBody, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		errorResponse := newErrorResponse("Unable to read request body", err)
+		fieldLogger.WithError(err).Warn(errorResponse.Message)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(errorResponse.JSON())
+		return
+	}
+	defer req.Body.Close()
+
 	// Prepare a proxy request:
-	// requestBody := req.Body
 	proxyURL := fmt.Sprintf("http://%s%s", destinationAddress, req.RequestURI)
-	proxyRequest, err := http.NewRequest(req.Method, proxyURL, nil)
+	proxyRequest, err := http.NewRequest(req.Method, proxyURL, bytes.NewReader(requestBody))
 	if err != nil {
 		errorResponse := newErrorResponse("Unable to build proxy request", err)
 		fieldLogger.WithError(err).Warn(errorResponse.Message)
@@ -58,7 +70,18 @@ func (p *Proxy) handler(w http.ResponseWriter, req *http.Request) {
 	}
 	defer proxyResponse.Body.Close()
 
+	// Read the response body:
+	responseBody, err := ioutil.ReadAll(proxyResponse.Body)
+	if err != nil {
+		errorResponse := newErrorResponse("Unable to read response body", err)
+		fieldLogger.WithError(err).Warn(errorResponse.Message)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(errorResponse.JSON())
+		return
+	}
+
 	// Forward the response:
 	fieldLogger.WithField("status_code", proxyResponse.StatusCode).Debug("Successfully proxied request")
 	w.WriteHeader(proxyResponse.StatusCode)
+	w.Write(responseBody)
 }
